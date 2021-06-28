aadcac8c089a8831ddda1d0b411c0ab7694678c8
Rhuan Gonzaga <62728188+Rhuan-Gonzaga@users.noreply.github.com>
2021-06-28 11:46:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function sortear (){
     let numero = document.getElementById("numero").value;
     let tela = document.getElementById("titulo");
-    let vencedor =  Math.floor(Math.random() * numero);
+    let vencedor =  Math.floor(Math.random() * numero) + 1;
 
     tela.innerText = "O Sorteado foi: " + vencedor;
 
